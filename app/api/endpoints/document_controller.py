203bb7aa5b4cb2f5daaa1feb89237a22493b838d
ujjwalgodara9 <ujjwal20345@iiitd.ac.in>
2025-04-29 08:26:38 +0530

    Logger removed

--- app/api/endpoints/document_controller.py
+++ app/api/endpoints/document_controller.py
@@ -19,16 +19,16 @@ async def compare_documents(request: CompareRequest):
     request_id = datetime.now().strftime("%Y%m%d_%H%M%S_%f")
     log_context = {'request_id': request_id}
 
-    logger.info("Starting document comparison", extra=log_context)
+    logger.info("Starting document comparison" )
 
     try:
         if len(request.captures) != 3:
             raise HTTPException(status_code=400, detail="Exactly three images are required for comparison.")
 
-        logger.info("Converting base64 images to PIL images", extra=log_context)
+        logger.info("Converting base64 images to PIL images" )
         images = [base64_to_image(capture.data) for capture in request.captures]
 
-        logger.info("Processing images with Gemini", extra=log_context)
+        logger.info("Processing images with Gemini")
         responses = await asyncio.gather(*[
             process_image(img, capture.url, idx + 1)
             for idx, (img, capture) in enumerate(zip(images, request.captures))
@@ -37,9 +37,16 @@ async def compare_documents(request: CompareRequest):
         comparison_result = compare_responses(responses)
 
         logger.info(
-            f"Comparison completed. Status: {comparison_result.get('status', 'Unknown')}",
-            extra=log_context
+            f"Comparison completed. Status: {comparison_result.get('status', 'Unknown')}"
         )
+        
+        logger.info("HEhehehehheheh")
+        logger.info(ComparisonResponse(
+            response1=responses[0].data,
+            response2=responses[1].data,
+            response3=responses[2].data,
+            comparison_result=comparison_result
+        ) )
 
         return ComparisonResponse(
             response1=responses[0].data,
@@ -72,16 +79,16 @@ async def compare_documents_openai(request: CompareRequest):
     request_id = datetime.now().strftime("%Y%m%d_%H%M%S_%f")
     log_context = {'request_id': request_id}
 
-    logger.info("Starting document comparison with Open API", extra=log_context)
+    logger.info("Starting document comparison with Open API" )
 
     try:
         if len(request.captures) != 3:
             raise HTTPException(status_code=400, detail="Exactly three images are required for comparison.")
 
-        logger.info("Converting base64 images to PIL images", extra=log_context)
+        logger.info("Converting base64 images to PIL images" )
         images = [base64_to_image(capture.data) for capture in request.captures]
 
-        logger.info("Processing images with OPEN API", extra=log_context)
+        logger.info("Processing images with OPEN API" )
         responses = await asyncio.gather(*[
             process_image(img, capture.url, idx + 1, llm="openai")
             for idx, (img, capture) in enumerate(zip(images, request.captures))
